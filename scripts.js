fa3c4bda7e9298e06e1dc879c6e0840f12f3681b
TheMattyDGWU <themattydgwu@gmail.com>
2020-12-04 19:24:50 -0500

    added hidden class functionality to containers

--- scripts.js
+++ scripts.js
@@ -1,37 +1,31 @@
 $("document").ready(function () {
 
-//CLICK Functions
-$("#homeButton").on("click", function () {
-     console.log("homeButton button has been clicked");
+     //CLICK Functions
+     $("#homeButton").on("click", function () {
+          console.log("homeButton button has been clicked");
 
-     $("#homeButton").addClass("hidden");
-     $("#recipe-display").addClass("hidden");
-     $("#wine-display").addClass("hidden");
-     $("#search-options").removeClass("hidden");
+          $("#indexContainer").removeClass("hidden");
+          $("#portfolioContainer").addClass("hidden");
+          $("#interestsContainer").addClass("hidden");
 
- });
+     });
 
- $("#portfolioButton").on("click", function () {
-     console.log("Recipe search button has been clicked");
+     $("#portfolioButton").on("click", function () {
+          console.log("Recipe search button has been clicked");
 
-     $("#homeButton").removeClass("hidden");
-     $("#recipe-display").removeClass("hidden");
-     $("#search-options").addClass("hidden");
+          $("#indexContainer").addClass("hidden");
+          $("#portfolioContainer").removeClass("hidden");
+          $("#interestsContainer").addClass("hidden");
 
- });
+     });
 
- $("#interestsButton").on("click", function () {
-     console.log("wine pairing button has been clicked");
+     $("#interestsButton").on("click", function () {
+          console.log("wine pairing button has been clicked");
 
-     $("#homeButton").removeClass("hidden");
-     $("#wine-display").removeClass("hidden");
-     $("#search-options").addClass("hidden");
+          $("#indexContainer").addClass("hidden");
+          $("#portfolioContainer").addClass("hidden");
+          $("#interestsContainer").removeClass("hidden");
 
- });
-
-
-
-
-
+     });
 
 });
